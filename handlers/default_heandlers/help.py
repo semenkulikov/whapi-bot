2f2ebc7badbb5869b398544172db2525573f559a
Semen Saifutdinov <saifutdinov2006@mail.ru>
2024-09-07 11:54:19 +0300

    Update help command

--- handlers/default_heandlers/help.py
+++ handlers/default_heandlers/help.py
@@ -6,5 +6,11 @@ from loader import bot
 
 @bot.message_handler(commands=['help'])
 def bot_help(message: Message):
-    text = [f'/{command} - {desk}' for command, desk in DEFAULT_COMMANDS]
-    bot.reply_to(message, 'Доступные команды:\n{}'.format("\n".join(text)))
+    if message.from_user.id in ALLOWED_USERS:
+        text = [f'/{command} - {desk}' for command, desk in DEFAULT_COMMANDS]
+        bot.reply_to(message, 'Доступные команды:\n{}'.format("\n".join(text)))
+    else:
+        bot.send_message(message.from_user.id, f"Здравствуйте, {message.from_user.full_name}! "
+                                               f"Я - телеграм бот. "
+                                               f"Вам доступны следующие команды:\n"
+                                               f"{'\n'.join(["/send_documents"])}")
